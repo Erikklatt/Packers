070d63b5cb5826bddea7d8d8a1829c21d8541fda
Erik Klatt <Erik.Klatt@Navegate.com>
2022-08-15 11:59:41 -0500

    Cleaning up dates, and adding more info

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,4 +1,5 @@
 import { Avatar, Stack } from "@mui/material";
+import moment from "moment/moment";
 import aaronJones from "../static/AaronJones.jpg";
 import aaronRodgers from "../static/AaronRodgers.jpg";
 import ajDillon from "../static/AjDillon.jpg";
@@ -27,24 +28,47 @@ export const PlayerInfo = (props) => {
         playerID,
         viewingPlayer
     } = props;
+    
     return (
         <>
             <div style={styles.playerInfo}>
             <div>
                 <Avatar sx={{ width: 125, height: 125 }} src={playerImage(playerID)} />
             </div>
-            <Stack style={styles.stack} spacing={2}>
-                <div>Name :</div>
-                <div>Team Name :</div>
-                <div>Weight :</div>
-                <div>Position :</div>
-            </Stack>
-            <Stack style={styles.stack} spacing={2}>
-                <div>{viewingPlayer.PlayerName || ''}</div>
-                <a style={{color: '#FFB612'}} href="https://www.packers.com/" target="_blank" rel="noreferrer"><div>{viewingPlayer.TeamName + ', ' + viewingPlayer.TeamAbbreviation  || ''}</div></a>
-                <div>{viewingPlayer.Wgt || ''}</div>
-                <div>{viewingPlayer.PositionId || ''}</div>
-            </Stack>
+            <div style={{display: 'flex'}}>
+              <div style={{display: 'flex'}}>
+                <Stack style={styles.stack} spacing={2}>
+                    <div>Name :</div>
+                    <div>Team Name :</div>
+                    <div>Position :</div>
+                    <div>Draft Team :</div>
+                    <div>Draft Year :</div>
+                    <div>College :</div>
+                </Stack>
+                <Stack style={styles.stack} spacing={2}>
+                    <div>{viewingPlayer.PlayerName + ' #' + viewingPlayer.Jersey|| ''}</div>
+                    <a style={{color: '#FFB612'}} href="https://www.packers.com/" target="_blank" rel="noreferrer"><div>{viewingPlayer.TeamName + ', ' + viewingPlayer.TeamAbbreviation  || ''}</div></a>
+                    <div>{viewingPlayer.PositionId || ''}</div>
+                    <div>{viewingPlayer.DraftTeam || ''}</div>
+                    <div>{viewingPlayer.DraftYear + ', Pick: ' + viewingPlayer.OverallDraftPick || ''}</div>
+                    <div>{viewingPlayer.SchoolName || ''}</div>
+                </Stack>
+              </div>
+              <div style={{display: 'flex'}}>
+                <Stack style={styles.stack} spacing={2}>
+                    <div>Date of Birth :</div>
+                    <div>Height :</div>
+                    <div>Weight :</div>
+                    <div>40 Time :</div>
+                </Stack>
+                <Stack style={styles.stack} spacing={2}>
+                    <div>{moment(viewingPlayer.DOB).format('MM-DD-YYYY') || ''}</div>
+                    <div>{viewingPlayer.Hgt || ''}</div>
+                    <div>{viewingPlayer.Wgt || ''}</div>
+                    <div>{viewingPlayer.Speed40 || ''}</div>
+                </Stack>
+              </div>
+            </div>
             </div>
         </>
     );
@@ -52,7 +76,7 @@ export const PlayerInfo = (props) => {
 
 const styles = {
     playerInfo: {
-      margin: '50px 0 100px 0',
+      margin: '16px 0 32px 0',
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'start',
